d71c5fb88174906a9df6c7d119fa5af51acbfc74
Isabela-Silverio-Cardoso-Pereira <isabelasilverio.c@gmail.com>
2024-03-25 13:49:05 -0300

    primeiro commit

---
+++ main (3).cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+potência simples
+
+*******************************************************************************/
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+
+double x,y;
+cin >> x; cin >> y;
+
+double conta = pow (x,y);
+cout << conta;
+
+
+
+    return 0;
+}
